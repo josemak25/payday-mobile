11364c6f0db4fa5edcb44db609249f13d49337fd
josemak25 <amakirij@gmail.com>
2020-10-27 18:11:28 +0100

    add signin screen component styles

---
+++ src/screens/signin/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+import { Title } from 'react-native-paper';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.BACKGROUND_COLOR};
+`;
+
+export const Welcome = styled(Title)`
+  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE}px;
+  font-family: ${({ theme }) => theme.fonts.SEN_BOLD};
+  color: ${({ theme }) => theme.colors.PRIMARY_TEXT};
+  text-transform: capitalize;
+`;
