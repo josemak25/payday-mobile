a9a6e7141cc8b656d6d5445d97712aa615417b52
josemak25 <amakirij@gmail.com>
2020-10-27 17:59:19 +0100

    add app constants

---
+++ src/constants/index.ts
@@ -0,0 +1,20 @@
+/*
+ ****************************************************************
+ ******************    ALL APP CONSTANTS   **********************
+ ****************************************************************
+ */
+
+// ALL ASYNC STORAGE CONSTANTS
+export const USER_CREDITS: string = "@USER_CREDITS";
+export const USER_FIRST_LAUNCH = "@FIRST_TIME_LAUNCH";
+
+// ALL PAGE LOGICAL CONSTANTS
+export const PAGINATION_DEFAULT: number = 15;
+
+// APP HEADER SETTING
+export const GLOBAL_HEADER_STYLE = {
+  shadowOpacity: 0,
+  shadowOffset: { height: 0, width: 0 },
+  shadowRadius: 0,
+  elevation: 0,
+};
