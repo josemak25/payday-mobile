fbc643bd92e1dd97afa1f0dd55841e40aaef50b8
josemak25 <amakirij@gmail.com>
2020-10-27 18:25:16 +0100

    add jest config and configured babel plugins

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: 'jest-expo',
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  moduleNameMapper: {
+    styledComponents:
+      'node_modules/styled-components/native/dist/styled-components.native.cjs.js'
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)'
+  ],
+  testPathIgnorePatterns: ['/node_modules/', '__tests__/types.d.ts']
+};
